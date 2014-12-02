50d2b429a4dbfb750e09c054689c459afc2b061b
Hadikusumo <john.hadikusumo@gmail.com>
2014-12-03 10:22:33 +1100

    Just Update
    Just Update

---
+++ MaharajaRestaurant/MaharajaRestaurant/Controllers/ReservationController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ReservationController.Controllers
+{
+    [RoutePrefix("Reservation")]
+    [AllowAnonymous]
+    public class ReservationController : Controller
+    {
+        [Route("")]
+        public ActionResult Index(string category)
+        {
+            return View();
+        }
+    }
+}
