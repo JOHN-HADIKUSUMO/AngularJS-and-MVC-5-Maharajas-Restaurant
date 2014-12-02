459b4e852e149ae00e5a0b1cee80ebe8884d833a
Hadikusumo <john.hadikusumo@gmail.com>
2014-12-02 21:21:14 +1100

    Tonight Update
    Tonight Update

--- MaharajaRestaurant/MaharajaRestaurant/Scripts/Maharajas/Menus/menusController.js
+++ MaharajaRestaurant/MaharajaRestaurant/Scripts/Maharajas/Menus/menusController.js
@@ -1,3 +1,9 @@
 ﻿maharajasApp.controller('menusController', function menusController($scope, menusService) {
     $scope.menuslist = menusService.menuslist;
+    $scope.add = function (x) {
+        alert(x);
+    };
+    $scope.read = function (x) {
+        alert(x);
+    };
 });
--- MaharajaRestaurant/MaharajaRestaurant/Scripts/Maharajas/Menus/menusService.js
+++ MaharajaRestaurant/MaharajaRestaurant/Scripts/Maharajas/Menus/menusService.js
@@ -1,81 +1,71 @@
 ﻿maharajasApp.factory('menusService', function () {
-    return {
-        menuslist: [
+    var getall = function () {
+        var temp = [
             [
                 {
                     id: 0,
-                    url: 'Images/Sliders/1.png',
+                    imgurl: '/Images/Boxes/300x300-Box.png',
                     title: 'Integer et pretium velit.',
                     description: 'Cras massa diam, cursus sit amet sagittis a, rhoncus ut nunc. Quisque sodales eros eget erat scelerisque sodales.',
-                    css: 'active'
+                    price: '$2.50 (3pcs)'
                 },
                 {
                     id: 1,
-                    url: 'Images/Sliders/2.png',
+                    imgurl: '/Images/Boxes/300x300-Box.png',
                     title: 'Sed ante nulla, molestie eu ipsum id.',
                     description: 'Etiam ac tempor tortor, id viverra massa. Vestibulum eros nunc, rutrum sed lacus et, ultrices pulvinar turpis.',
-                    css: ''
+                    price: '$2.50 (3pcs)'
                 },
                 {
                     id: 2,
-                    url: 'Images/Sliders/3.png',
+                    imgurl: '/Images/Boxes/300x300-Box.png',
                     title: 'Quisque a arcu tortor.',
                     description: ' Etiam sed ultricies metus, at condimentum neque. Nulla ac gravida lacus.',
-                    css: ''
+                    price: '$2.50 (3pcs)'
                 },
                 {
                     id: 3,
-                    url: 'Images/Sliders/4.png',
+                    imgurl: '/Images/Boxes/300x300-Box.png',
                     title: 'Fusce posuere tristique mi et.',
                     description: 'Sed semper nisl sit amet quam interdum mollis ac ac dui.',
-                    css: ''
-                },
+                    price: '$2.50 (3pcs)'
+                }
+            ],
+            [
                 {
                     id: 4,
-                    url: 'Images/Sliders/5.png',
-                    title: 'Pellentesque habitant morbi tristique senectus et netus.',
-                    description: 'Quisque purus enim, suscipit sed tincidunt at, placerat vitae sapien.',
-                    css: ''
-                }
-
-            ],
-            [           
-                {
-                    id: 5,
-                    url: 'Images/Sliders/1.png',
+                    imgurl: '/Images/Boxes/300x300-Box.png',
                     title: 'Integer et pretium velit.',
                     description: 'Cras massa diam, cursus sit amet sagittis a, rhoncus ut nunc. Quisque sodales eros eget erat scelerisque sodales.',
-                    css: 'active'
+                    price: '$2.50 (3pcs)'
+                },
+                {
+                    id: 5,
+                    imgurl: '/Images/Boxes/300x300-Box.png',
+                    title: 'Sed ante nulla, molestie eu ipsum id.',
+                    description: 'Etiam ac tempor tortor, id viverra massa. Vestibulum eros nunc, rutrum sed lacus et, ultrices pulvinar turpis.',
+                    price: '$2.50 (3pcs)'
                 },
                 {
                     id: 6,
-                    url: 'Images/Sliders/2.png',
-                    title: 'Sed ante nulla, molestie eu ipsum id.',
-                    description: 'Etiam ac tempor tortor, id viverra massa. Vestibulum eros nunc, rutrum sed lacus et, ultrices pulvinar turpis.',
-                    css: ''
+                    imgurl: '/Images/Boxes/300x300-Box.png',
+                    title: 'Quisque a arcu tortor.',
+                    description: ' Etiam sed ultricies metus, at condimentum neque. Nulla ac gravida lacus.',
+                    price: '$2.50 (3pcs)'
                 },
                 {
                     id: 7,
-                    url: 'Images/Sliders/3.png',
-                    title: 'Quisque a arcu tortor.',
-                    description: ' Etiam sed ultricies metus, at condimentum neque. Nulla ac gravida lacus.',
-                    css: ''
-                },
-                {
-                    id: 8,
-                    url: 'Images/Sliders/4.png',
+                    imgurl: '/Images/Boxes/300x300-Box.png',
                     title: 'Fusce posuere tristique mi et.',
                     description: 'Sed semper nisl sit amet quam interdum mollis ac ac dui.',
-                    css: ''
-                },
-                {
-                    id: 9,
-                    url: 'Images/Sliders/5.png',
-                    title: 'Pellentesque habitant morbi tristique senectus et netus.',
-                    description: 'Quisque purus enim, suscipit sed tincidunt at, placerat vitae sapien.',
-                    css: ''
+                    price: '$2.50 (3pcs)'
                 }
             ]
         ]
+
+        return temp;
+    };
+    return {
+        menuslist: getall()
     }
 });
